d7a296d9a67346838a0d7e513c7dd5ae5f60aa45
André Sampaio <andresampaio@id.uff.br>
2023-08-27 15:57:58 -0300

    Escapar decorator

--- app/src/views/view.ts
+++ app/src/views/view.ts
@@ -4,17 +4,13 @@ import { logarTempoDeExecucao } from "../decorators/logar-tempo-de-execucao.js";
 export abstract class View<T> {
 
     protected elemento: HTMLElement;
-    private escapar = false;
 
-    constructor(seletor: string, escapar?: boolean) {
+    constructor(seletor: string) {
         const elemento = document.querySelector(seletor);
         if (elemento) {
             this.elemento = elemento as HTMLElement;
         }else{
             throw Error(`Selector ${seletor} does not exist in DOM. Please verify.`)
-        }
-        if(escapar){
-            this.escapar=escapar;
         }
     }
 
@@ -22,9 +18,6 @@ export abstract class View<T> {
     @inspect
     public update(model: T): void {
         let template = this.template(model);
-        if(this.escapar){
-            template = template.replace(/<script>[\s\S]*?<script>/, '');
-        }
         this.elemento.innerHTML = template;
     }
 
