ba24dd72cec55072c924d3fea7f95cbb74665139
André Sampaio <andresampaio@id.uff.br>
2023-08-27 15:28:44 -0300

    Decorator inspect

--- app/dist/js/decorators/logar-tempo-de-execucao.js
+++ app/dist/js/decorators/logar-tempo-de-execucao.js
@@ -1,11 +1,17 @@
-export function logarTempoDeExecucao() {
+export function logarTempoDeExecucao(emSegundos = false) {
     return function (target, propertyKey, descriptor) {
         const metodoOriginal = descriptor.value;
         descriptor.value = function (...args) {
+            let divisor = 1;
+            let unidades = 'miliseconds';
+            if (!emSegundos) {
+                let divisor = 1000;
+                let unidades = 'seconds';
+            }
             const t1 = performance.now();
             const retorno = metodoOriginal.apply(this, args);
             const t2 = performance.now();
-            console.log(`${propertyKey}, runtime: ${(t2 - t1) / 1000} seconds.`);
+            console.log(`${propertyKey}, runtime: ${(t2 - t1) / divisor} ${unidades}.`);
             retorno;
         };
         return descriptor;
--- app/src/decorators/logar-tempo-de-execucao.ts
+++ app/src/decorators/logar-tempo-de-execucao.ts
@@ -1,4 +1,4 @@
-export function logarTempoDeExecucao(){
+export function logarTempoDeExecucao(emSegundos: boolean = false){
     return function(
         target: any,
         propertyKey: string,
@@ -6,10 +6,16 @@ export function logarTempoDeExecucao(){
     ){
         const metodoOriginal =  descriptor.value;
         descriptor.value = function(...args: any[]){
+            let divisor = 1;
+            let unidades = 'miliseconds';
+            if(!emSegundos){
+                let divisor = 1000;
+                let unidades = 'seconds';
+            }
             const t1 = performance.now();
             const retorno = metodoOriginal.apply(this, args);
             const t2 = performance.now();
-            console.log(`${propertyKey}, runtime: ${(t2-t1)/1000} seconds.`);
+            console.log(`${propertyKey}, runtime: ${(t2-t1)/divisor} ${unidades}.`);
             retorno;
         };
         return descriptor;
