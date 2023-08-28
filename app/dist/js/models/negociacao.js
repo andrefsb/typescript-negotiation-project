4ac71480e44866e3551bc44e08c69af5e3a1be22
André Sampaio <andresampaio@id.uff.br>
2023-08-28 14:28:11 -0300

    Printing negociations and new negotiation

--- app/dist/js/models/negociacao.js
+++ app/dist/js/models/negociacao.js
@@ -1,5 +1,7 @@
-export class Negociacao {
+import { Imprimivel } from "../utils/imprimivel.js";
+export class Negociacao extends Imprimivel {
     constructor(_data, quantidade, valor) {
+        super();
         this._data = _data;
         this.quantidade = quantidade;
         this.valor = valor;
@@ -8,14 +10,21 @@ export class Negociacao {
         const data = new Date(this._data.getTime());
         return data;
     }
-    get volume() {
-        return this.quantidade * this.valor;
-    }
     static criaDe(dataString, quantidadeString, valorString) {
         const exp = /-/g;
         const date = new Date(dataString.replace(exp, ','));
         const quantidade = parseInt(quantidadeString);
         const valor = parseFloat(valorString);
         return new Negociacao(date, quantidade, valor);
+    }
+    paraTexto() {
+        return `
+            Date: ${this.data},
+            Quantity: ${this.quantidade},
+            Value: ${this.valor}
+        `;
+    }
+    get volume() {
+        return this.quantidade * this.valor;
     }
 }
--- app/src/models/negociacao.ts
+++ app/src/models/negociacao.ts
@@ -1,18 +1,18 @@
-export class Negociacao { 
+import { Imprimivel } from "../utils/imprimivel.js";
+
+export class Negociacao extends Imprimivel { 
 
     constructor (
         private _data: Date,
         public readonly quantidade: number,
         public readonly valor: number
-    ) {}
+    ) {
+        super();
+    }
 
     get data(): Date {
         const data = new Date(this._data.getTime());
         return data;
-    }
-
-    get volume(): number{
-        return this.quantidade*this.valor;
     }
 
     public static criaDe(dataString: string, quantidadeString: string, valorString: string){
@@ -22,5 +22,19 @@ export class Negociacao {
         const valor = parseFloat(valorString);
         return new Negociacao(date, quantidade, valor);
     }
+
+    public paraTexto(): string{
+        return `
+            Date: ${this.data},
+            Quantity: ${this.quantidade},
+            Value: ${this.valor}
+        `;
+    }
+
+    get volume(): number{
+        return this.quantidade*this.valor;
+    }
+
+  
 
 }
